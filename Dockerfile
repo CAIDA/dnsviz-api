16c398f3ab7b4d34def3e0cd4f1d53f5e9278715
Vinay Pillai <vinaypillai01@gmail.com>
2021-04-07 23:38:46 -0700

    feat(trusttree.py): add reverse link data stemming from domains dependent on a given parent domain

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ EXPOSE 5000
 
 WORKDIR /app/dnsviz_api
 
-CMD gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
+CMD gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --worker-connections 1000 --timeout 5 --keep-alive 5 --log-level info
--- dnsviz_api/app/resources/TrustTree.py
+++ dnsviz_api/app/resources/TrustTree.py
@@ -14,6 +14,7 @@ TRUST_TREE_QUERY = '''
             ip4 as ip4_glue
             ip6 as ip6_glue
             pd as parentDomain
+            node_uid as uid
         }
         var(func: eq(descriptor, $descriptor)){
             id as uid
@@ -22,6 +23,12 @@ TRUST_TREE_QUERY = '''
             rp as ~authNsParent
             rc as ~authNsChild
         }
+        var(func: uid(node_uid)){
+            rpd as ~parentDomain (first:5) @filter(eq(dgraph.type, nameserver)){
+                pd_rp as ~authNsParent
+                pd_rc as ~authNsChild 
+            }
+        }
         q(func: uid(p,c, pd, id, ip4, ip6)){
             uid
             dgraph.type
@@ -29,7 +36,7 @@ TRUST_TREE_QUERY = '''
                 uid
             }
         }
-        borders(func: uid(rp, rc)){
+        domain_borders(func: uid(rp, rc, pd_rp, pd_rc)){
             uid
             xid
             dgraph.type,
@@ -39,6 +46,16 @@ TRUST_TREE_QUERY = '''
                 uid
             }
             authNsChild{
+                uid
+            }
+        }
+        ns_borders(func: uid(rpd)){
+            uid
+            xid
+            dgraph.type,
+            descriptor
+            isMapped
+            parentDomain{
                 uid
             }
         }
@@ -59,6 +76,15 @@ class TrustTree(Resource):
             descriptor = f'{descriptor}.'
         hostname_data = db.query(TRUST_TREE_QUERY, {'$descriptor':descriptor})
         if len(hostname_data['q']) > 0:
-            return hostname_data
+            parsed_hostname_data = {
+                'q':hostname_data['q']
+            }
+            parsed_hostname_data['borders'] = []
+            border_type_list = ('domain_borders', 'ns_borders')
+            for border_type in border_type_list:
+                if border_type in hostname_data:
+                    for node in hostname_data[border_type]:
+                        parsed_hostname_data['borders'].append(node)
+            return parsed_hostname_data
         else:
             return invalid_hostname_error()
